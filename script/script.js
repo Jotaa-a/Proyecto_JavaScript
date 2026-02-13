d095cdcd91173920a163ce7905bbbd42b964505c
Jotaa-a <jmantillavillalba208@gmail.com>
2026-02-13 08:30:54 -0500

    :sparkles: Se inició al menos

---
+++ script/script.js
@@ -0,0 +1,63 @@
+let estudiantes =[
+    {
+      id: "001",
+      nombre: "Carlos",
+      apellido: "Ramírez",
+      direccion: "Calle 12 #45-67",
+      acudiente: "María Ramírez",
+      telefonoCelular: "3001234567",
+      telefonoFijo: "6012345678",
+      estado: "Activo",
+      riesgo: "N/A"
+    },
+    {
+      id: "002",
+      nombre: "Laura",
+      apellido: "Gómez",
+      direccion: "Carrera 8 #23-45",
+      acudiente: "Jorge Gómez",
+      telefonoCelular: "3012345678",
+      telefonoFijo: "6023456789",
+      estado: "Activo",
+      riesgo: "N/A"
+    },
+    {
+      id: "003",
+      nombre: "Andrés",
+      apellido: "Martínez",
+      direccion: "Av. Siempre Viva 742",
+      acudiente: "Patricia Martínez",
+      telefonoCelular: "3023456789",
+      telefonoFijo: "6034567890",
+      estado: "Inactivo",
+      riesgo: "N/A"
+    },
+    {
+      id: "004",
+      nombre: "Sofía",
+      apellido: "López",
+      direccion: "Calle 50 #10-20",
+      acudiente: "Ricardo López",
+      telefonoCelular: "3034567890",
+      telefonoFijo: "6045678901",
+      estado: "Activo",
+      riesgo: "N/A"
+    },
+    {
+      id: "005",
+      nombre: "Miguel",
+      apellido: "Hernández",
+      direccion: "Carrera 15 #30-55",
+      acudiente: "Ana Hernández",
+      telefonoCelular: "3045678901",
+      telefonoFijo: "6056789012",
+      estado: "Activo",
+      riesgo: "N/A"
+    }
+  ];
+
+
+let user = prompt("Ingrese su usuario")
+let password = prompt("Ingrese su contraseña")
+// console.log("Biemvenido " + nombre + " al perfil de: " + cargo)
+console.log(typeof password)
